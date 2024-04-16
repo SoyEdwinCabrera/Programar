0f4e4f9452db6c65a78f53d415949d67ef384548
SoyEdwinCabrera <edwinleeroth@gmail.com>
2024-04-15 19:00:59 -0500

    Commit 18

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,4 +1,5 @@
 let ataqueJugador
+let ataqueEnemigo
 
 
 function iniciarJuego() {
@@ -33,12 +34,12 @@ function seleccionarMascotaJugador() {
 }
 
 function seleccionarMascotaEnemigo() {
-    let ataqueAleatorio = aleatorio(1,3)
+    let mascotaAleatorio = aleatorio(1,3)
     let spanMascotaEnemigo = document.getElementById('mascota-enemigo')
 
-    if (ataqueAleatorio == 1) {
+    if (mascotaAleatorio == 1) {
         spanMascotaEnemigo.innerHTML = 'Hipodoge'
-    } else if (ataqueAleatorio == 2) {
+    } else if (mascotaAleatorio == 2) {
         spanMascotaEnemigo.innerHTML = 'Capipepo'
     } else {
         spanMascotaEnemigo.innerHTML = 'Ratigueya'
@@ -47,15 +48,26 @@ function seleccionarMascotaEnemigo() {
 
 function ataqueFuego() {
     ataqueJugador = 'FUEGO'
-    alert('ataqueJugador')
+    ataqueAleatorioEnemigo()
 }
 function ataqueAgua() {
     ataqueJugador = 'AGUA'
-    alert('ataqueJugador')
+    ataqueAleatorioEnemigo()
 }
 function ataqueTierra() {
     ataqueJugador = 'TIERRA'
-    alert('ataqueJugador')
+    ataqueAleatorioEnemigo()
+}
+function ataqueAleatorioEnemigo() {
+    let ataqueAleatorio = aleatorio(1,3)
+
+    if (ataqueAleatorio == 1) {
+        ataqueEnemigo = 'FUEGO'
+    } else if (ataqueAleatorio == 2) {
+        ataqueEnemigo = 'AGUA'
+    } else {
+        ataqueEnemigo = 'TIERRA'
+    }
 }
 
 function aleatorio(min, max) {
